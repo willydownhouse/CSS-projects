7c420afb09031bcee42cfc86a7a75d50cea78138
willydownhouse <ville.l.alatalo@gmail.com>
2021-05-08 19:32:15 +0200

    😊

---
+++ MenuTest/script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".burger");
+const beef = document.querySelector(".beef");
+
+burger.addEventListener("click", (e) => {
+  burger.classList.toggle("open");
+});
