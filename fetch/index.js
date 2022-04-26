e329a39a99b6c5f0edac9e5c3acea103a75031d7
willydownhouse <ville.l.alatalo@gmail.com>
2022-04-26 18:56:31 +0200

    �

--- fetch/index.js
+++ fetch/index.js
@@ -10,28 +10,53 @@ const url2 =
   'https://images.pexels.com/photos/7363341/pexels-photo-7363341.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
 const createImage = url => {
-  const img = document.createElement('img');
-
-  img.src = url;
-
-  img.addEventListener('load', () => {
-    console.log('image loaded');
-    img.style.opacity = 1;
-    box.appendChild(img);
-  });
-
   return new Promise((resolve, reject) => {
-    resolve(img);
+    console.log('creating image');
+    const img = document.createElement('img');
+
+    img.src = url;
+
+    img.addEventListener('load', () => {
+      console.log('image loaded');
+      box.appendChild(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', () => {
+      reject(new Error('Error loading image'));
+    });
   });
 };
 
-createImage(url)
-  .then(img => {
-    console.log(img);
-    setTimeout(() => {
-      img.style.opacity = 0;
-      console.log('img go away');
-    }, 2000);
-  })
-  //.then(img => console.log('second img', img))
-  .catch(err => console.log(err));
+// createImage(url)
+//   .then(() => wait(2))
+//   .then(() => {
+//     box.innerHTML = '';
+//     return wait(2);
+//   })
+//   .then(() => createImage('ddsadsad'))
+//   .then(() => wait(2))
+//   .then(() => (box.innerHTML = ''))
+//   .catch(err => console.log(err));
+
+const renderImages = async () => {
+  try {
+    await createImage(url);
+    await wait(2);
+    box.innerHTML = '';
+    await wait(2);
+    await createImage(url2);
+    await wait(2);
+    box.innerHTML = '';
+  } catch (err) {
+    console.log(err, `💥💥💥`);
+  }
+};
+
+renderImages();
+
+function wait(seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
