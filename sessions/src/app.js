c18c46a742c64848dca5ca98a53b7f0c4b6ee80e
willydownhouse <ville.l.alatalo@gmail.com>
2022-06-23 09:59:39 +0200

    �

--- sessions/src/app.js
+++ sessions/src/app.js
@@ -1,47 +1,70 @@
 const express = require("express");
 const path = require("path");
-const session = require("express-session");
+const sessions = require("express-session");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
 
-const options = {
-  root: path.join(__dirname),
-};
+const oneDay = 1000 * 60 * 60 * 24;
 
-app.get("/", (req, res) => {
-  res.sendFile("public/html/index.html", options, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-});
+app.use(
+  sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
+
+// cookie parser middleware
+app.use(cookieParser());
 
 const user = {
   name: "keke",
   pass: "test1234",
 };
 
+let session;
+
+// VIEWS
+app.get("/", (req, res) => {
+  res.sendFile("public/html/index.html", { root: __dirname });
+});
+app.get("/home", (req, res) => {
+  session = req.session;
+  if (session.userid) {
+    res.sendFile("public/html/home.html", { root: __dirname });
+  } else {
+    res.redirect("/");
+  }
+});
+app.get("/fail", (req, res) => {
+  res.sendFile("public/html/fail.html", { root: __dirname });
+});
+
+//LOGIN
 app.post("/login", (req, res) => {
   console.log(req.body);
 
   const { username, password } = req.body;
 
   if (user.name === username && user.pass === password) {
-    res.sendFile("public/html/home.html", options, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  } else {
-    res.sendFile("public/html/fail.html", options, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    session = req.session;
+    session.userid = username;
+    console.log("SUCCESFULL SIGN IN:");
+    console.log(req.session);
+    return res.status(200).send("succesfull log in");
   }
+
+  res.status(401).send("wrong username or password");
+});
+
+// LOGOUT
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.send("you logged out");
 });
 
 app.all("*", (req, res) => {
